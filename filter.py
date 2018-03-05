d95901af94cc82437de1f6b16a3da2e36fe7b1be
imcauley <mcauleyisaac@gmail.com>
2018-03-05 10:46:11 -0700

    edge filter

--- filter.py
+++ filter.py
@@ -12,8 +12,8 @@ while(1):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # define range of blue color in HSV
-    lower_blue = np.array([0,50,50])
-    upper_blue = np.array([255,255,130])
+    lower_blue = np.array([100,100,20])
+    upper_blue = np.array([150,150,255])
 
     # Threshold the HSV image to get only blue colors
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
@@ -25,7 +25,7 @@ while(1):
     cv2.imshow('mask',mask)
     cv2.imshow('res',res)
     k = cv2.waitKey(5) & 0xFF
-    if k == 27:
+    if k == 'q':
         break
 
 cv2.destroyAllWindows()
--- test.py
+++ test.py
@@ -8,15 +8,11 @@ while(True):
 
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
 
-    # define range of blue color in HSV
-    lower_blue = np.array([110,50,50])
-    upper_blue = np.array([130,255,255])
+    edges = cv2.Canny(rgb,100,200)
 
-    # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(rgb, lower_blue, upper_blue)
 
-    cv2.imshow('frame', mask)
-    if cv2.waitKey(0) & 0xFF == ord('q'):
+    cv2.imshow('frame', edges)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         out = cv2.imwrite('capture.jpg', frame)
         break
 
